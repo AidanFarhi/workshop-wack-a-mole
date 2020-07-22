31077751d420e62563bce63057e26470ca13fb66
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-07-22 15:39:19 -0400

    first commit

--- index.js
+++ index.js
@@ -3,4 +3,23 @@
 let score = 0;
 var scoreDisplay = document.getElementById('score');
 
-console.log(scoreDisplay);
+var holes = Array.from(document.getElementsByClassName('hole'))
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+setInterval(function() {
+    let randomHoleIndex = Math.floor(getRandom(0, holes.length))
+    console.log(randomHoleIndex)
+    return holes[randomHoleIndex].classList.toggle('mole')
+}, 300)
+
+const gameArea = document.getElementById('whack-a-mole')
+
+gameArea.addEventListener('click', function(click) {
+    if (click.target.matches('.mole')) {
+        console.log('hit!!')
+    }
+});
+

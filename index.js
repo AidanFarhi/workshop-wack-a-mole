b38ca4a03e83607271abc7366062f8f340d9e2a2
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-07-22 16:08:42 -0400

    cleaned up js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var scoreDisplay = document.getElementById('score');
 var holes = Array.from(document.getElementsByClassName('hole'))
 const gameArea = document.getElementById('whack-a-mole')
 
-// this function gets us a randam number in a range
+// this function gets us a random number in a range
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 }
@@ -13,9 +13,8 @@ function getRandom(min, max) {
 // this fires whatever we put here at an interval set by us
 setInterval(function() {
     let randomHoleIndex = Math.floor(getRandom(0, holes.length))
-    console.log(randomHoleIndex)
-    return holes[randomHoleIndex].classList.toggle('mole')
-}, 300)
+    holes[randomHoleIndex].classList.toggle('mole')
+}, 1000)
 
 // this is where the action happens
 gameArea.addEventListener('click', function(click) {

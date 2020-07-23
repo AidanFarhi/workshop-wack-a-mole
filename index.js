745df6029356da248e05dc678dce045530ff7a4d
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-07-23 08:09:17 -0400

    added in a special mole. Need to add in speed toggle and fix mole overlap

--- index.js
+++ index.js
@@ -4,23 +4,59 @@ let score = 0;
 var scoreDisplay = document.getElementById('score');
 var holes = Array.from(document.getElementsByClassName('hole'))
 const gameArea = document.getElementById('whack-a-mole')
+const speedToggle = document.getElementById('speed-buttons')
+var speed = 700
 
-// this function gets us a random number in a range
+// random num
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
 };
 
-//this fires whatever we put inside at an interval set by us
+//this populates our regular moles every .7 seconds
+var populate = setInterval(function() {
+
+    let randomHoleIndex = Math.floor(getRandom(0, holes.length))
+    let randomHoleIndex2 = Math.floor(getRandom(0, holes.length))
+    let randomHoleIndex3 = Math.floor(getRandom(0, holes.length))
+    let randomHoleIndex4 = Math.floor(getRandom(0, holes.length))
+    holes[randomHoleIndex].classList.toggle('mole')      
+    holes[randomHoleIndex2].classList.toggle('mole')    
+    holes[randomHoleIndex3].classList.toggle('mole')      
+    holes[randomHoleIndex4].classList.toggle('mole')  
+     
+}, 700);
+
+// lets make a special mole pop up every 5 seconds
 setInterval(function() {
     let randomHoleIndex = Math.floor(getRandom(0, holes.length))
-    holes[randomHoleIndex].classList.toggle('mole')
-}, 2000);
+    holes[randomHoleIndex].classList.toggle('special')
+}, 5000)
 
-// this is where the action happens
+// gets rid of image when clicked, and adds to score
 gameArea.addEventListener('click', function(click) {
     if (click.target.matches('.mole')) {
         score++
         scoreDisplay.innerHTML = score
         click.target.classList.toggle('mole')
+    } else if (click.target.matches('.special')) {
+        score += 100
+        scoreDisplay.innerHTML = score
+        click.target.classList.toggle('special')
     }
-});
+})
+
+// speedToggle.addEventListener('click', function(click) {
+//     if (click.target.matches('.slow')) {
+//         console.log('hit slow')
+//         console.log('speed:', speed)
+//         speed = 5000
+//     } else if (click.target.matches('.normal')) {
+//         console.log('hit normal')
+//         console.log('speed:', speed)
+//         speed = 700
+//     } else if (click.target.matches('.fast')) {
+//         console.log('hit fast')
+//         console.log('speed:', speed)
+//         speed = 100
+//     }
+// })
